620fe6d14c158a8324a6f48e24dd0f3e7ad05f62
Michael Glicken <michael.glicken@gmail.com>
2018-04-02 10:51:23 -0500

    Adjusted User Routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Myapp::Application.routes.draw do
 
   devise_for :users
  # You can have the root of your site routed with "root"
-  root 'people#user_dashboard'
+
   get "/admin/", :controller => "accesses", :action => "admin"
   get "/models/", :controller => "people", :action => "models"
   get "/download/:id", :controller => "accesses", :action => "download_data"
