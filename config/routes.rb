3f105d61d7db31bd378c555af4d132351aa99a01
Michael Glicken <michael.glicken@gmail.com>
2018-04-02 10:53:22 -0500

    Fixed User Routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Myapp::Application.routes.draw do
 
   devise_for :users
  # You can have the root of your site routed with "root"
+  root 'people#user_dashboard'
 
   get "/admin/", :controller => "accesses", :action => "admin"
   get "/models/", :controller => "people", :action => "models"
