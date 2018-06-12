9b6a5186513a8a803d84d30ec14ce9fbcdb77c03
Michael Glicken <michael.glicken@gmail.com>
2018-06-12 10:11:32 -0500

    subdomain cleanup

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,13 +14,6 @@ Devise.setup do |config|
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
 
-
-  # Devise subdomain setup
-  Devise.setup do |config|
-    config.mailer.class_eval do 
-      helper :subdomain 
-    end
-  end
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
