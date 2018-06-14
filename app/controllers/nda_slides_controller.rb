ab600ac6fae5bf59048c1b9b678e8658c7a97ec4
Michael Glicken <michael.glicken@gmail.com>
2018-06-14 11:43:32 -0500

    NDA Slide Controller Update

--- app/controllers/nda_slides_controller.rb
+++ app/controllers/nda_slides_controller.rb
@@ -1,4 +1,4 @@
-class NDASlidesController < ApplicationController
+class NdaSlidesController < ApplicationController
 
 before_action :ensure_admin_access,  only: [:index, :show, :import]
 before_action :ensure_banker_access,  only: [:new, :create, :edit, :update, :destroy]
