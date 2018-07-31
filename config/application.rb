5afd8cc59b38e9ffdb8f90199680bd6e4e1f385c
Michael Glicken <michael.glicken@gmail.com>
2018-07-31 11:39:43 -0500

    Removed aspose.Slides-Cloud 2018.07.31

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ require 'axlsx'
 require "csv"
 require 'rubygems'
 require 'json'
-require 'aspose_slides_cloud'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
